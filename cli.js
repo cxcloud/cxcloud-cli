72048ba2eb7960e7cae31964ea2b1b5f9be96660
Sallar Kaboli <sallar.kaboli@gmail.com>
2018-05-07 14:21:27 +0300

    Remove extra deps

--- cli.js
+++ cli.js
@@ -1,15 +1,11 @@
 #!/usr/bin/env node
 "use strict";
 
-const importJsx = require("import-jsx");
-const { h, render } = require("ink");
 const meow = require("meow");
-
-const Ui = importJsx("./ui");
 
 const cli = meow(`
   Usage
-    $ cxcloud-cli [input]
+    $ cxcloud-cli <input>
 
   Options
     --name  Lorem ipsum [Default: false]
@@ -21,4 +17,4 @@ const cli = meow(`
     I love ponies
 `);
 
-render(h(Ui, cli.flags));
+console.log(cli);
